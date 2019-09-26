6b758baac8c57b513acd499f80de02e900b3d63c
DEEPAK SHARMA <deepak.sharma1@girnarsoft.com>
2019-09-26 10:37:46 +0530

    Init project setup

---
+++ NeoniosDemo/NeoniosDemo.h
@@ -0,0 +1,19 @@
+//
+//  NeoniosDemo.h
+//  NeoniosDemo
+//
+//  Created by Deepak Sharma on 25/09/19.
+//  Copyright © 2019 GirnarSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeoniosDemo.
+FOUNDATION_EXPORT double NeoniosDemoVersionNumber;
+
+//! Project version string for NeoniosDemo.
+FOUNDATION_EXPORT const unsigned char NeoniosDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeoniosDemo/PublicHeader.h>
+
+
---
+++ NeoniosDemo/ViewController.swift
@@ -0,0 +1,16 @@
+
+//
+//  ViewController.swift
+//  Neon-Ios
+//
+//  Created by Akhilendra Singh on 1/16/19.
+//  Copyright © 2019 Girnar. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+
+class ViewController: UIViewController {
+    
+}
