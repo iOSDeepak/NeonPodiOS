aaa1f252144dcdf356801478cbb32e0273cb4fff
DEEPAK SHARMA <deepak.sharma1@girnarsoft.com>
2019-09-26 12:57:03 +0530

    Init project setup

--- NeoniosDemo/NeoniosDemo.h
+++ NeoniosDemo/NeoniosDemo.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <NeoniosDemo/TOCropViewController.h>
+#import <TOCropViewController.h>
 
 //! Project version number for NeoniosDemo.
 FOUNDATION_EXPORT double NeoniosDemoVersionNumber;
